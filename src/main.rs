0d1c37ee14746d41536d6d94f103c0a704ed3452
Otávio Pace <otaviopp8@gmail.com>
2018-02-02 07:43:27 -0200

    Temperature converter working :)

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ use std::io;
 fn main() {
     println!("Temperature converter");
 
-    io::stdin().read_line(&mut unit)
-        .expect("Failed to read line");
-
     loop {
         println!("Choose which unit you have: (c or C for Celsius, f or F for Fahrenheit)");
 
@@ -14,29 +11,51 @@ fn main() {
         io::stdin().read_line(&mut unit)
             .expect("Failed to read line");
 
-        if unit != "c" && unit != "C" && unit != "f" && unit != "F" {
-            continue;
+        let unit = unit.trim();
+
+        match unit {
+            "c" | "C" | "f" | "F" => {
+                loop {
+                    println!("Type the value you want to convert");
+
+                    let mut temperature = String::new();
+
+                    io::stdin().read_line(&mut temperature)
+                        .expect("Failed to read line");
+
+                    let temperature: f32 = match temperature.trim().parse() {
+                        Ok(temp) => temp,
+                        Err(_) => continue,
+                    };
+
+                    let constant = 32.0;
+
+                    let result: f32 = match unit {
+                        "c" | "C" => temperature * 1.8 + constant,
+                        "f" | "F" => (temperature - constant) * 0.5556,
+                        _ => {
+                            println!("Wierd you've got here");
+                            0.0
+                        },
+                    };
+
+                    let new_unit = match unit {
+                        "c" | "C" => "F",
+                        "f" | "F" => "C",
+                        _ => {
+                            println!("Wierd you've got here");
+                            ""
+                        },
+                    };
+
+                    println!("You've got {}º{}", result, new_unit);
+                    break;
+                }
+
+                break;
+            },
+            _ => continue,
         }
 
-        loop {
-            let mut temperature = String::new();
-
-            io::stdin().read_line(&mut temperature)
-                .expect("Failed to read line");
-
-            let temperature: f32 = match temperature.trim().parse() {
-                Ok(temp) => temp,
-                Err(_) => continue,
-            };
-
-            let result: f32 = match unit {
-                "c" | "C" => temperature * 1.8 + 32,
-                "f" | "F" => (temperature - 32) * 0.5556,
-            };
-
-            println!("You've got {}º", result);
-        }
-
-        break;
     }
 }
